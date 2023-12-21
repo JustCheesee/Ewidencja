06717c3e1ebbf321921d01f0936122e2bdb7f30f
JustCheesee <danielgaida01@gmail.com>
2023-12-21 16:11:50 +0100

    Delete main method from Application interface

--- src/Application.java
+++ src/Application.java
@@ -5,25 +5,6 @@ import java.util.Scanner;
 
 public interface Application
 {
-    static void main(String[] args) throws IOException
-    {
-        System.out.println("Jak chcesz się zalogować?\n");
-        System.out.println("1. Obywatel");
-        System.out.println("2. Urzędnik");
-
-        Scanner in = new Scanner(System.in);
-        int input = in.nextInt();
-        switch (input)
-        {
-            case 1:
-                CitizenView citizen = new CitizenView();
-                break;
-            case 2:
-                ClerkView clerk = new ClerkView();
-                break;
-        }
-
-    }
     boolean login(long PESEL, String password);
     boolean verifyData(long PESEL, LocalDate birthDate, boolean sex);
     ArrayList<Record> showData(Record criteria);
